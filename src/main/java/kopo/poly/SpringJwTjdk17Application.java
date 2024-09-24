9f242fd50bb7a55e9517a9e9b75dd5679439fa19
Geuniv <kkoowl99@gmail.com>
2024-09-24 14:03:14 +0900

    2024.09.24 JWT 강의 ( 이협건 교수님 )

---
+++ src/main/java/kopo/poly/SpringJwTjdk17Application.java
@@ -0,0 +1,13 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwTjdk17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwTjdk17Application.class, args);
+    }
+
+}
